f1dd3431b2e00556471ac494a532f539644c0fff
Teker932 <70298466+eputnam0522@users.noreply.github.com>
2024-04-26 15:42:30 +0000

    commit 2

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # copyleft dobrostvari, 2024
+
+import ping
+import socket
+import random as rand
+
+def rand_string(length):
+    i = length
+
+    while i != 0:
+        out += '\u' + hex(rand.randint(0, 65535))
+        i -= 1
+
+def crawl():
+    while true:
+        try:
+            ping.verbose_ping(rand_string(rand.randint(0, 63)))
